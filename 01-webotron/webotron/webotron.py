faf57ece7c7a2734c0e7f49e9c36aa30073e035f
javier <parkerpeter634@yahoo.com>
2018-08-04 01:26:34 -0400

    on the way in

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,5 +1,6 @@
 import boto3
 import click
+from botocore.exceptions import ClientError
 
 
 session = boto3.Session(profile_name='pythonAutomation')
@@ -22,6 +23,44 @@ def list_bucket_objects(bucket):
     "Help list on the objects"
     for obj in s3.Bucket(bucket).objects.all():
         print(obj)
+
+@cli.command('setup-bucket')
+@click.argument('bucket')
+def setup_bucket(bucket):
+    """ Raising the limits """
+    try:
+        s3_bucket = s3.create_bucket(Bucket=bucket, CreateBucketConfiguration={'LocationConstraint': session.region_name})
+
+        policy = """
+        {
+          "Id": "Policy1533357798500",
+          "Version": "2012-10-17",
+          "Statement": [{
+          "Sid": "Stmt1533357790398",
+          "Action": "s3:*",
+          "Effect": "Allow",
+          "Resource": "arn:aws:s3:::{}/*",
+          "Principal": {
+            "AWS": [
+              "*"
+                ]
+              }
+            }
+          ]
+        }
+        """.format(s3_bucket)
+        policy = policy.strip()
+        pol = s3_bucket.Policy()
+        pol.put(Policy=policy)
+        ws = new_bucket.Website()
+        ws.put(WebsiteConfiguration={"ErrorDocument": {"Key": "error.html"}, "IndexDocument": {"Suffix": "index.html"}})
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+            s3_bucket = s3.Bucket(bucket)
+            print(s3_bucket)
+        else:
+            raise e
+
 
 if __name__ == '__main__':
     cli()
